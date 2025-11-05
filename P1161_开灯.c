e62d2a44dda93e0fea547cdbc522c182a8ebfbc2
ineedvpn767 <qq13767708646@163.com>
2025-11-05 11:00:43 +0800

    原来是这样修改

---
+++ P1161_开灯.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    double a;
+    int t;
+    int i,j;
+    int ans = 0;
+    for (i = 0;i < n;i++) {
+        scanf("%lf %d",&a,&t);
+        for (j = 1;j <= t;j++) {
+            ans = ans ^ (int)floor(j*a);
+        }
+    }
+
+    printf("%d",ans);
+    return 0;
+}
